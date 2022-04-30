e61e37f8ddaac1f1f039e410cd9ef8d8989fc127
Brandon <cohbrandon@gmail.com>
2022-04-30 01:03:50 -0400

    added click action with popup modal

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -5,8 +5,8 @@ import Text from "../Cards/Text";
 const Body = () => {
   return (
     <div className="pt-32 sm:pt-40">
-      <div className="container flex flex-row items-center justify-center min-h-full py-12 mx-auto sm:py-24">
-        <div className="flex-row items-start justify-center w-11/12 mb-48 sm:w-2/3 sm:flex sm:mb-12">
+      <div className="container flex flex-col items-center justify-center min-h-full py-12 mx-auto sm:py-24">
+        <div className="flex-col items-center justify-center w-11/12 mb-48 sm:w-2/3 sm:flex sm:mb-12">
           <TextWithImage />
           <Text />
         </div>
--- src/components/Cards/Text.js
+++ src/components/Cards/Text.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import basicText from "../../../basictext.json";
+import TextPopup from "../Modals/TextPopup";
 
 const Text = () => {
+  const [show, setShow] = useState(false);
+
   return (
     <div className="w-11/12 mb-48 sm:w-2/3 sm:mb-12">
       <h1
-        className={`mt-2 text-[${basicText.font_size}] font-[${basicText.font_weight}] text-${basicText.alignment} text-[${basicText.color}] cursor-default`}
+        className={`mt-2 cursor-pointer text-[${basicText.font_size}] font-[${basicText.font_weight}] text-${basicText.alignment} text-[${basicText.color}] cursor-default`}
+        onClick={() => setShow(!show)}
       >
         {basicText.text}
       </h1>
+      <TextPopup className={`${show ? "block" : "hidden"}`} show={show} />
     </div>
   );
 };
